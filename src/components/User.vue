c882c597cf4c4a538e913d7f7ae5afda8ec028a9
Benson <benson@Benson.local>
2018-03-05 17:33:25 +0800

    add: 用户首页

--- src/components/User.vue
+++ src/components/User.vue
@@ -1,9 +1,32 @@
 <template>
   <layout :has_share="has_share" title="我的">
-    <div class="page_wrap">
-      <div class="page_bd">
-        sadf
-      </div>
+    <div class="wrap">
+      <mu-list>
+        <mu-list-item title="观看历史" to="user/history">
+          <mu-icon slot="left" value="history"/>
+          <mu-icon slot="right" value="keyboard_arrow_right"/>
+        </mu-list-item>
+        <mu-list-item title="我的收藏" to="user/favorite">
+          <mu-icon slot="left" value="favorite"/>
+          <mu-icon slot="right" value="keyboard_arrow_right"/>
+        </mu-list-item>
+        <mu-list-item title="设置" to="user/settings">
+          <mu-icon slot="left" value="settings"/>
+          <mu-icon slot="right" value="keyboard_arrow_right"/>
+        </mu-list-item>
+        <mu-list-item title="关于" to="user/about">
+          <mu-icon slot="left" value="timeline"/>
+          <mu-icon slot="right" value="keyboard_arrow_right"/>
+        </mu-list-item>
+        <mu-list-item title="登录" to="user/login">
+          <mu-icon slot="left" value="timeline"/>
+          <mu-icon slot="right" value="keyboard_arrow_right"/>
+        </mu-list-item>
+        <mu-list-item title="测试" to="user/test">
+          <mu-icon slot="left" value="timeline"/>
+          <mu-icon slot="right" value="keyboard_arrow_right"/>
+        </mu-list-item>
+      </mu-list>
     </div>
   </layout>
 </template>
